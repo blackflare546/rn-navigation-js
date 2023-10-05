c1567dfe88258ad2df57f3cec1dae42bf14e81d5
Glenn Mark Flores <gflores@hyperstacksinc.com>
2023-10-05 11:48:47 +0800

    feat: update bottom tab navigator

--- src/@core/Navigation.js
+++ src/@core/Navigation.js
@@ -1,16 +1,33 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Feed, Notificaiton, Settings } from "../screens/BottomTabs";
+import { Ionicons } from "@expo/vector-icons";
 
-const Tab = createBottomTabNavigator();
+const BottomTab = createBottomTabNavigator();
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Feed} />
-      <Tab.Screen name="Notification" component={Notificaiton} />
-      <Tab.Screen name="Settings" component={Settings} />
-    </Tab.Navigator>
+    <BottomTab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === "Feed") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "ios-settings-sharp";
+          } else if (route.name === "Notification") {
+            iconName = focused ? "ios-notifications" : "notifications-outline";
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "#1DA1F2",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <BottomTab.Screen name="Feed" component={Feed} />
+      <BottomTab.Screen name="Notification" component={Notificaiton} />
+      <BottomTab.Screen name="Settings" component={Settings} />
+    </BottomTab.Navigator>
   );
 }
 
